3b0cf0c7da483eaa263c20600145590a40d572c5
Arman <mdarman365506@gmail.com>
2024-11-25 18:06:59 +0600

    added updated option

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,9 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users = useLoaderData();
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
 
     const handleDelete = (_id) => {
         console.log('delete', _id);
@@ -11,8 +13,10 @@ const Users = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                if (data.deleteCount > 0) {
+                if (data.deletedCount>0) {
                     alert('deleted successfully!')
+                    const remaining = users.filter(user => user._id !== _id);
+                    setUsers(remaining);
                 }
         })
     }
@@ -23,9 +27,11 @@ const Users = () => {
           {users.map((user) => (
             <p key={user._id}>
               {" "}
-                  {user.name}: {user.email} {user._id}: <button
-                  onClick={()=> handleDelete(user._id)}
-                  >X</button>
+              {user.name}: {user.email} {user._id}:
+              <Link to={`/update/${user._id}`}>
+                <button>Update</button>
+              </Link>
+              <button onClick={() => handleDelete(user._id)}>X</button>
             </p>
           ))}
         </div>
