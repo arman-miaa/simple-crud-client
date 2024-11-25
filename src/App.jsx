a5b7a0aaf6811f36e0fcb9a33d783cde47b2acd9
Arman <mdarman365506@gmail.com>
2024-11-25 15:08:18 +0600

    get data from database

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,11 @@ function App() {
     })
       .then(res => res.json())
       .then(data => {
-      console.log(data);
+        console.log(data);
+        if (data.insertedId) {
+          alert("Users added successsfully");
+          form.reset();
+        }
     })
 }
   return (
